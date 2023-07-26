e7a8d19be59640cdf0a6834b913798c2d5b4daf6
lucilaperezb <133135893+lucilaperezb@users.noreply.github.com>
2023-07-26 05:56:54 +0000

    commits

--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def inicio(request):
+
+
+    return HttpResponse("vista inicio")
+
+
+def cursos(request):
+
+
+    return HttpResponse("vista cursos")
+
+
+def profesores(request):
+
+
+    return HttpResponse("vista profesores")
+
+
+def estudiantes(request):
+
+
+    return HttpResponse("vista estudiantes")
+
+
+def entregables(request):
+
+
+    return HttpResponse("vista entregables")
